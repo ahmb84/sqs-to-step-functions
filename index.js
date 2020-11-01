050be470b1a54cab9f3be768fb9c00c11411760a
Mokhtar Bacha <mokhtar@kantum.xyz>
2020-11-01 19:13:05 +0200

    first commit

--- index.js
+++ index.js
@@ -1,40 +1,16 @@
 const aws = require('aws-sdk')
 const stepfunctions = new aws.StepFunctions({ region: 'us-east-1' })
 
-const stepFunctionsArns =
-  {
-    plaid: 'arn:aws:states:us-east-1:537508223032:stateMachine:plaid-transactions-event',
-    belvo: 'arn:aws:states:us-east-1:537508223032:stateMachine:belvo-transactions-event',
-    yapily: 'arn:aws:states:us-east-1:537508223032:stateMachine:yapily-transactions-event',
-    enable: 'arn:aws:states:us-east-1:537508223032:stateMachine:enable-transactions-event'
-  }
+const stateMachineArn = 'arn:aws:states:us-east-1:537508223032:stateMachine:standard-bank-aggregator-transactions-event-pipeline'
 
 exports.handler = async (event) => {
   try {
-    for (const { body, messageAttributes } of event.Records) {
-      await callStepFunction(messageAttributes, body)
-      const response = {
-        statusCode: 200,
-        body: `Successfully processed ${event.Records.length} messages.`
-      }
-      return response
+    for (const { body } of event.Records) {
+      await stepfunctions.startExecution({ stateMachineArn, input: body }).promise()
     }
   } catch (error) {
     console.error('An error occured while calling the step functions')
     console.error(error)
-    const response = {
-      statusCode: 500,
-      body: `Failed to process ${event.Records.length} messages.`
-    }
-    return response
+    throw error
   }
-}
-
-async function callStepFunction (messageAttributes, body) {
-  const apiAggregor = messageAttributes.ApiAggregor.stringValue
-  const params = {
-    stateMachineArn: stepFunctionsArns[apiAggregor],
-    input: body
-  }
-  await stepfunctions.startExecution(params).promise()
 }
